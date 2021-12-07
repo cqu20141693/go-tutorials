9686e7bdd764bca55af572a191fa75ec14dd73ee
wujiangtao <wujiangtao@chongctech.com>
2021-12-07 09:24:36 +0800

    test strings Contains Func

---
+++ test/string_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestIndex(t *testing.T) {
+	logDir1 := ""
+	logDir2 := "log"
+	logDir3 := "/var/log"
+
+	checkDir(logDir1)
+	checkDir(logDir2)
+	checkDir(logDir3)
+}
+
+func checkDir(dir string) {
+	if dir == "" {
+		fmt.Println("empty")
+	} else if strings.Contains(dir, "/") {
+		fmt.Println("contains /")
+	} else {
+		fmt.Println("else")
+	}
+
+}
