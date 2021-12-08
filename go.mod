8feb302d4f8ea0a7bdaa94c3af98f9cd91b58af0
wujiangtao <wujiangtao@chongctech.com>
2021-12-08 17:21:05 +0800

    add logger.go

--- go.mod
+++ go.mod
@@ -7,48 +7,4 @@ require (
 	github.com/gin-gonic/gin v1.7.7
 	go.uber.org/zap v1.19.1
 )
-
-require (
-	github.com/aliyun/alibaba-cloud-sdk-go v1.61.976 // indirect
-	github.com/buger/jsonparser v0.0.0-20181115193947-bf1c66bbce23 // indirect
-	github.com/fsnotify/fsnotify v1.5.1 // indirect
-	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-errors/errors v1.0.1 // indirect
-	github.com/go-playground/locales v0.13.0 // indirect
-	github.com/go-playground/universal-translator v0.17.0 // indirect
-	github.com/go-playground/validator/v10 v10.4.1 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/json-iterator/go v1.1.11 // indirect
-	github.com/leodido/go-urn v1.2.0 // indirect
-	github.com/lestrrat/go-file-rotatelogs v0.0.0-20180223000712-d3151e2a480f // indirect
-	github.com/lestrrat/go-strftime v0.0.0-20180220042222-ba3bf9c1d042 // indirect
-	github.com/magiconair/properties v1.8.5 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/mitchellh/mapstructure v1.4.2 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/nacos-group/nacos-sdk-go v1.0.9 // indirect
-	github.com/pelletier/go-toml v1.9.4 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/spf13/afero v1.6.0 // indirect
-	github.com/spf13/cast v1.4.1 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.9.0 // indirect
-	github.com/subosito/gotenv v1.2.0 // indirect
-	github.com/toolkits/concurrent v0.0.0-20150624120057-a4371d70e3e3 // indirect
-	github.com/ugorji/go/codec v1.1.7 // indirect
-	go-micro.dev/v4 v4.4.0 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
-	golang.org/x/text v0.3.6 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
-	gopkg.in/ini.v1 v1.63.2 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-)
-
 replace github.com/cqu20141693/go-service-common => ../go-service-common
---
+++ log/logger.go
@@ -0,0 +1,123 @@
+package logger
+
+import (
+	"context"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// LogLevel holds an AtomicLevel that can be used to change the logging
+// level of Default.
+var LogLevel = zap.NewAtomicLevel()
+
+// Default holds the logger returned by Logger when there is no logger in
+// the context. If replacing Default with a new Logger then consider
+// using &LogLevel as the LevelEnabler so that SetLevel can still be used
+// to dynamically change the logging level.
+var Default = zap.New(
+	zapcore.NewCore(
+		zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+			MessageKey:  "msg",
+			LevelKey:    "level",
+			TimeKey:     "ts",
+			EncodeLevel: zapcore.LowercaseLevelEncoder,
+			EncodeTime:  zapcore.ISO8601TimeEncoder,
+		}),
+		os.Stdout,
+		&LogLevel,
+	),
+)
+
+func SetLogLevel(level zap.AtomicLevel) {
+	LogLevel = level
+}
+
+// loggerKey holds the context key used for loggers.
+type loggerKey struct{}
+
+// WithLogger returns a new context derived from ctx that
+// is associated with the given logger.
+func WithLogger(ctx context.Context, logger *zap.Logger) context.Context {
+	return context.WithValue(ctx, loggerKey{}, logger)
+}
+
+// WithFields returns a new context derived from ctx
+// that has a logger that always logs the given fields.
+func WithFields(ctx context.Context, fields ...zapcore.Field) context.Context {
+	return WithLogger(ctx, Logger(ctx).With(fields...))
+}
+
+// WithLevel returns a new context derived from ctx
+// that has a logger that only logs messages at or above
+// the given level.
+func WithLevel(ctx context.Context, level zapcore.Level) context.Context {
+	return WithLogger(ctx, Logger(ctx).WithOptions(wrapCoreWithLevel(level)))
+}
+
+func wrapCoreWithLevel(level zapcore.Level) zap.Option {
+	return zap.WrapCore(func(core zapcore.Core) zapcore.Core {
+		return &coreWithLevel{
+			Core:  core,
+			level: level,
+		}
+	})
+}
+
+type coreWithLevel struct {
+	zapcore.Core
+	level zapcore.Level
+}
+
+func (c *coreWithLevel) Enabled(level zapcore.Level) bool {
+	return c.level.Enabled(level) && c.Core.Enabled(level)
+}
+
+func (c *coreWithLevel) Check(e zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore.CheckedEntry {
+	// We only need to do the local level check because
+	// c.Core will do its own level checking.
+	if !c.level.Enabled(e.Level) {
+		return ce
+	}
+	return c.Core.Check(e, ce)
+}
+
+func (c *coreWithLevel) With(fields []zap.Field) zapcore.Core {
+	return &coreWithLevel{
+		Core:  c.Core.With(fields),
+		level: c.level,
+	}
+}
+
+// Logger returns the logger associated with the given
+// context. If there is no logger, it will return Default.
+func Logger(ctx context.Context) *zap.Logger {
+	if ctx == nil {
+		panic("nil context passed to Logger")
+	}
+	if logger, _ := ctx.Value(loggerKey{}).(*zap.Logger); logger != nil {
+		return logger
+	}
+	return Default
+}
+
+// Debug calls Logger(ctx).Debug(msg, fields...).
+func Debug(ctx context.Context, msg string, fields ...zapcore.Field) {
+	Logger(ctx).Debug(msg, fields...)
+}
+
+// Info calls Logger(ctx).Info(msg, fields...).
+func Info(ctx context.Context, msg string, fields ...zapcore.Field) {
+	Logger(ctx).Info(msg, fields...)
+}
+
+// Warn calls Logger(ctx).Warn(msg, fields...).
+func Warn(ctx context.Context, msg string, fields ...zapcore.Field) {
+	Logger(ctx).Warn(msg, fields...)
+}
+
+// Error calls Logger(ctx).Error(msg, fields...).
+func Error(ctx context.Context, msg string, fields ...zapcore.Field) {
+	Logger(ctx).Error(msg, fields...)
+}
