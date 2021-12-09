c1fc900cd567b9906f08e22483c4e47a6aa032c1
wujiangtao <wujiangtao@chongctech.com>
2021-12-09 18:25:06 +0800

    add viper test

--- webapp.go
+++ webapp.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	"github.com/cqu20141693/go-service-common/boot"
+	"fmt"
+	"github.com/cqu20141693/go-service-common/config"
+	"github.com/cqu20141693/go-service-common/event"
 	"github.com/cqu20141693/go-service-common/global"
 	"github.com/cqu20141693/go-service-common/logger/cclog"
+	"github.com/spf13/viper"
 	"go.uber.org/zap/zapcore"
-	"os"
 )
 
 func init() {
-	cclog.SetLevel(zapcore.DebugLevel)
 	global.SetLogLevel(zapcore.DebugLevel)
+	config.Init()
 }
 func main() {
-
-	boot.Boot(os.Args)
+	event.TriggerEvent(event.Start)
+	aKey := "cc.profiles.active"
+	bKey := "cc.log.dir"
+	nKey := "cc.application.name"
+	sKey := "server.port"
+	sprintf := fmt.Sprintf("read local over. %s =%s, %s=%s %s=%s %s=%s", aKey, viper.GetString(aKey), bKey, viper.GetString(bKey), nKey, viper.GetString(nKey), sKey, viper.GetString(sKey))
+	cclog.Debug(sprintf)
 }
