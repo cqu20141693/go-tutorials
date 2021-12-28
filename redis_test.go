1f8842f0cc8f1f6dc0c687165b811c368b1f9e18
wujiangtao <wujiangtao@chongctech.com>
2021-12-28 16:37:08 +0800

    add redis strMGet,pipeline,hashIncr  test

--- redis_test.go
+++ redis_test.go
@@ -8,6 +8,7 @@ import (
 	credis "github.com/cqu20141693/go-tutorials/redis"
 	"github.com/go-redis/redis/v8"
 	"log"
+	"reflect"
 	"strconv"
 	"testing"
 	"time"
@@ -86,7 +87,7 @@ func TestHashMGet(t *testing.T) {
 	key := "sipc:1234"
 	fields := []string{"callId", "fTag", "tTag"}
 	maps := []string{"callId", "1", "fTag", "wq", "tTag", "cc"}
-	err, done := mGet(key, fields)
+	err, done := hMGet(key, fields)
 	if done {
 		return
 	}
@@ -94,11 +95,11 @@ func TestHashMGet(t *testing.T) {
 	if err != nil {
 		return
 	}
-	mGet(key, fields)
+	hMGet(key, fields)
 
 }
 
-func mGet(key string, fields []string) (error, bool) {
+func hMGet(key string, fields []string) (error, bool) {
 	result, err := ccredis.RedisDB.HMGet(context.Background(), key, fields...).Result()
 	if err != nil {
 		return nil, true
@@ -116,4 +117,71 @@ func mGet(key string, fields []string) (error, bool) {
 
 	fmt.Println(callId, fTag, tTag)
 	return err, false
+}
+
+func TestStrMGet(t *testing.T) {
+	// MGet 只能是获取string
+	result, err := ccredis.RedisDB.MGet(context.Background(), "key1", "key2", "sip:test").Result()
+	if err != nil {
+		return
+	}
+	fmt.Println(result)
+}
+
+func TestPipeline(t *testing.T) {
+	pipe := ccredis.RedisDB.Pipeline()
+	ctx := context.Background()
+	keys := []string{"key1", "ke2", "sip:test"}
+	values := make([]interface{}, len(keys))
+	for i, key := range keys {
+		values[i], _ = pipe.HGetAll(ctx, key).Result()
+	}
+	cmds, err := pipe.Exec(ctx)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(values)
+	for i, cmd := range cmds {
+
+		mapCmd := cmd.(*redis.StringStringMapCmd)
+		values[i] = mapCmd.Val()
+	}
+	fmt.Println(cmds)
+	fmt.Println(values)
+
+}
+
+func TestHashIncr(t *testing.T) {
+	key := "sip:test"
+	incr, err := ccredis.RedisDB.HIncrBy(context.Background(), key, "CSeq", 1).Result()
+	if err != nil {
+		return
+	}
+	fmt.Println("hash incr=", incr)
+
+	result, err := ccredis.RedisDB.HGet(context.Background(), key, "CSeq").Result()
+	if err != nil {
+		return
+	}
+	fmt.Println("hash get ", result, reflect.TypeOf(result))
+}
+
+func TestPipelineV2(t *testing.T) {
+	keys := []string{"key1", "ke2", "sip:test"}
+	values := make([]map[string]string, len(keys))
+	ctx := context.Background()
+	cmders, err := ccredis.RedisDB.Pipelined(ctx, func(pipe redis.Pipeliner) error {
+		for i, key := range keys {
+			values[i], _ = pipe.HGetAll(ctx, key).Result()
+		}
+		return nil
+	})
+	if err != nil {
+		return
+	}
+	for i, cmd := range cmders {
+		mapCmd := cmd.(*redis.StringStringMapCmd)
+		values[i] = mapCmd.Val()
+	}
+	fmt.Println(values)
 }
